4b7b165a8e4a945e19e1d75db31fd9a136ebd05b
snehasing20 <snehasing20@gmail.com>
2024-10-24 15:26:31 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "production" {
-  ami                    = "ami-0e86e20dae9224db8"
-  key_name               = "asg"
+  ami                    = "ami-0da424eb883458071"
+  key_name               = "terraforminstances"
   vpc_security_group_ids = ["sg-038344d919e2dc655"]  
   instance_type          = "t2.micro"
   count                  = 1
